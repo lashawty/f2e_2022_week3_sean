c9b756f13f921944874ce4ae3bee7e002fd723b9
Sean <hustle0985@gmail.com>
2022-11-18 14:32:09 +0800

    1118 view1 done

--- js/app.js
+++ js/app.js
@@ -1,24 +1,50 @@
 const loading = function () {
-  gsap.to(".rocket", {
+  const tl = gsap.timeline();
+  tl.to(".rocket", {
     top:'0',
     yPercent:'-30',
     scale:0,
     duration: 5,
   })
-  gsap.to(".rocket-firing", {
+  .to(".rocket-firing", {
     top:'0',
     yPercent:'-30',
     scale:0,
     duration: 5,
-  })
-  gsap.to(".star-field", {
+  },"<")
+  .to(".star-field", {
     opacity:0,
     duration: 5,
-  })
-  gsap.to(".bg-star", {
+  },"<")
+  .to(".bg-star", {
     opacity: 1,
+    duration: 5,
+  },"<")
+  .to(".welcome", {
+    yPercent:'-10',
+    opacity:1,
+    duration: 5,
+  })
+  .to(".welcome-content", {
+    text: "哈囉，歡迎加入 TT 資訊！<br \>在正式加入專案開發之前，需要請你先了解 Scrum 的流程與精神！<br \>請接受挑戰任務，成功通過 Scrum 新手村的挑戰任務吧～", 
     duration: 10,
+    ease: "none",
+  },"<")
+  .to(".challenge-accepted", {
+    opacity:1,
+    duration: .1,
   })
 }
 
+const ajaxIntro = function () {
+  $.ajax({
+    type: "get",
+    url: "./intro.html",
+    data: "data",
+    dataType: "dataType",
+    success: function (response) {
+      
+    }
+  });
+}
 loading()
